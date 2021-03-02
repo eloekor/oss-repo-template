33967d67b7f4f0c2741300617ea6f5f93e880244
Elo <eloekor@gmail.com>
2021-03-01 22:28:11 -0500

    Lab 5 work

---
+++ labs/lab-05/Lab5.md
@@ -0,0 +1,28 @@
+# Lab 05 Report - Build Systems
+
+# 1
+### ![Step1Pic](step1Pic.jpg) 
+[Modified tutorial.cxx](Step1/tutorial.cxx)
+[CMakeLists.txt](Step1/CMakeLists.txt)
+
+# 2
+### ![Step2Pic](step2Pic.jpg) 
+[Modified tutorial.cxx](Step2/tutorial.cxx)
+[CMakeLists.txt](Step2/CMakeLists.txt)
+
+# 3
+### ![Step3Pic](step3Pic.jpg) 
+[Modified Math Function](Step3/MathFunctions/CMakeLists.txt)
+[CMakeLists.txt](Step2/CMakeLists.txt)
+
+# 4
+### ![Step4Pic](Step4Pic.jpg) 
+### ![Step4TestPic](Step4TestPic.jpg) 
+[Modified Math Function](Step4/MathFunctions/CMakeLists.txt)
+[CMakeLists.txt](Step4/CMakeLists.txt)
+
+#5
+### ![Step5Pic](step5Pic.jpg) 
+[Modified Math Function](Step5/MathFunctions/CMakeLists.txt)
+[CMakeLists.txt](Step5/CMakeLists.txt)
+  
---
+++ labs/lab-05/step5_build/MathFunctions/CMakeFiles/MathFunctions.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/users/ekore/Desktop/OSS_2021/oss-repo-template/labs/lab-05/cmake/help/guide/tutorial/Step5/MathFunctions/mysqrt.cxx" "/mnt/c/users/ekore/Desktop/OSS_2021/oss-repo-template/labs/lab-05/cmake/help/guide/tutorial/step5_build/MathFunctions/CMakeFiles/MathFunctions.dir/mysqrt.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
